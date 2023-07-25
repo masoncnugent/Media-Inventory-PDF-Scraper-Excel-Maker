83faaa2e945823dba319d827cf3064ecd0dc56fe
masoncnugent <115777359+masoncnugent@users.noreply.github.com>
2023-07-25 16:20:23 -0700

    Update PDF_Main.py
    Everything but graphs is working.

--- Inventory Python Files/PDF_Main.py
+++ Inventory Python Files/PDF_Main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from PDF_Class import data_scraper
 from PDF_To_Excel import excel_batch_processor
 
@@ -10,26 +11,40 @@ def directory_changer():
     #manual version
     #return os.chdir(input("Where are the inventory files stored?\n"))
 
-    #work version
+    #work version H drive
     #return os.chdir(r"P:\Public\Microbiology\Media Prep\Media Inventory\2023")
+
+    #work version C drive
+    return os.chdir(r"C:\Users\MCN2226\inventory speed test")
 
     #home desktop version
     #return os.chdir(r"C:\Users\Mason\Documents\NAMSA Test\Inventory")
 
     #home laptop version
-    return os.chdir(r"C:\Users\mason\OneDrive\Documents\Python Projects\Inventory Files")
+    #return os.chdir(r"C:\Users\mason\OneDrive\Documents\Python Projects\Inventory Files")
 
 
 
 #data_scraper calls ---> smart_splitter whose product calls ---> read_ahead
 
 def run_program():
+    start_time = datetime.now().time()
+    start_seconds = (start_time.hour * 60 + start_time.minute) * 60 + start_time.second
+
     pdf_location = directory_changer()
 
     data_scraper(pdf_location)
 
     excel_batch_processor()
 
+    end_time = datetime.now().time()
+    end_seconds = (end_time.hour * 60 + end_time.minute) * 60 + end_time.second
+
+    print("\n~" + str(end_seconds- start_seconds) + " seconds of compute")
+    print("If times are unsatisfactory, move network inventory files to a local drive")
+
 run_program()
 
 #One assumption is that the first pdf is formatted ideally, since it's media types are used to format the rest of the data into cells for use in graphs
+
+#the work pc is slow due to the time needed to load each pdf from the shared storage, not due to processing times
