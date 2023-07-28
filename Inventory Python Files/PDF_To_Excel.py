171913613663163acfe92ee4565f9f2efbf3f3b1
masoncnugent <115777359+masoncnugent@users.noreply.github.com>
2023-07-27 18:24:23 -0700

    Update PDF_To_Excel.py
    Functional chart making.

--- Inventory Python Files/PDF_To_Excel.py
+++ Inventory Python Files/PDF_To_Excel.py
@@ -29,7 +29,7 @@ def excel_pdf_vertical_copier(ws):
     pdf_offset = 1
 
     #moves the dates where each pdf's data is collected to start at 'I2' and continue rightward
-    date_offset = 9
+    date_offset = 3
 
     #iterates through every pdf in formatted_data
     for pdf in PDF.pdf_list:
@@ -60,108 +60,112 @@ def excel_pdf_vertical_copier(ws):
         ws[title_cell] = pdf.filename
 
         #adds the dates that all the inventory will be compared against
-        date_cell = get_column_letter(date_offset) + "2"
+        date_cell = "H" + str(date_offset)
         ws[date_cell] = pdf.filename[:8]
 
         if date_cell == "CA":
             continue
 
         date_offset += 1
+    
+    #adds the word "Date" above its respective column
+    ws["H2"] = "Date"
+
 
 
 #works from the first pdf, assuming it was formatted correctly and identically to all other pdfs
 #frameshift issues occur when this assumption is not met
 def excel_media_type_adder(ws):
 
+    #adds the word "Media Type" above its respective column
+    ws["I1"] = "Media Type"
+
+    #start at I2 and move to J2, K2, etc.
+
     #the first two ranges handle the cases where SCDB and FTM have to be added to the media type titles
+    #there is an assumption that nothing new will be added into SCDB and FTM, built into the lengths of each section being hardcoded
     for i in range(3, 11):
-        ws["H" + str(i)] = ws["A3"].value + " " + ws["B" + str(i)].value
+        ws[get_column_letter(i + 6) + "2"] = ws["A3"].value + " " + ws["B" + str(i)].value
+        PDF.pdf_media_type_list.append(ws["A3"].value + " " + ws["B" + str(i)].value)
 
     for i in range(11, 19):
-        ws["H" + str(i)] = ws["A11"].value + " " + ws["B" + str(i)].value
+        ws[get_column_letter(i + 6) + "2"] = ws["A11"].value + " " + ws["B" + str(i)].value
+        PDF.pdf_media_type_list.append(ws["A11"].value + " " + ws["B" + str(i)].value)
 
-    for i in range(19, 33):
-        ws["H" + str(i)] = ws["A" + str(i)].value + " " + ws["B" + str(i)].value
-
+    #the end is dynamically encoded to match the length of each PDF
+    for i in range(19, PDF.pdf_length + 2):
+        ws[get_column_letter(i + 6) + "2"] = ws["A" + str(i)].value + " " + ws["B" + str(i)].value
+        PDF.pdf_media_type_list.append(ws["A" + str(i)].value + " " + ws["B" + str(i)].value)
 
 
 #this can be re-written using pdf.inventory_list, which would greatly reduce errors and enhance readability
 def excel_pdf_inventory_copier(ws):
 
     #this is where the data starts to be printed
-    col_data_offset = 9
+    row_data_offset = 3
 
     for pdf in PDF.pdf_list:
-        row_data_offset = 3
-        col_let = get_column_letter(col_data_offset)
+        col_data_offset = 9
 
         inv_list = pdf.inventory_list
 
         for inv_val in inv_list:
+            col_let = get_column_letter(col_data_offset)
             ws[col_let + str(row_data_offset)] = inv_val
 
             #the row changes to allow each inventory value for a pdf to be printed
-            row_data_offset += 1
+            col_data_offset += 1
         
         #the column changes once each pdf has their values pasted
-        col_data_offset += 1
+        row_data_offset += 1
 
 
 
 #this is still in the testing phases, as specifying data for use in the graphs has poor documentation for openpyxl
 def excel_graph_maker(wb, ws):
-    
+
+    #starts at the eighth column, or H
+    graph_col_offset = 8
+    graph_row_offset = 0
     #dateaxis might be an import from openpyxl.chart.axis
     #I think with dateaxis your 'dates' cells have to be in a formatting it can turn into a true date
     #this would format and scale better as Excel is treating the axis as one with special date properties.
-    for i in range(1, PDF.pdf_id):
+
+    #PDF.length - 1 is equivalent to the amount of different media types
+    for i in range(1, PDF.pdf_length):
         line_chart = LineChart()
-        line_chart.title = "Test Inventory"
+        #takes the title from the list of media types given as a class attribute of PDF, dynamically
+        line_chart.title = PDF.pdf_media_type_list[i - 1]
         line_chart.x_axis.title = "Date"
         line_chart.y_axis.title = "Inventory"
 
         #experiment with what this 'crossAx' thing is...
-        #line_chart.x_axis = DateAxis()
         #line_chart.x_axis = DateAxis(crossAx=100)
 
-        #this mcustom number format could break things
+        #this custom number format could break things
         #line_chart.x_axis.number_format = "yy-mm-dd"
 
         #line_chart.x_axis.majorTimeUnit = "days"
 
-        #PDF.pdf_id might have to be +8 or something else to work
         #data
-        y_values = Reference(ws, min_col = 9, min_row = i + 2, max_col = PDF.pdf_id + 7, max_row = i + 2)
+        #changed to min_row = 2 with titles_from_data = True
+        y_values = Reference(ws, min_col = 8 + i, min_row = 2, max_col = 8 + i, max_row = PDF.pdf_id + 2)
 
         #categories
-        x_values = Reference(ws, min_col = 9, min_row = 2, max_col = PDF.pdf_id + 7, max_row = 2)
-
-        #experiment with what 'titles_from_data' does
-        #this should add the y-values
-        #if it's from_rows that's the answer I'm gonna be so mad
-        line_chart.set_categories(x_values)
+        x_values = Reference(ws, min_col = 8, min_row = 3, max_col = 8, max_row = PDF.pdf_id + 2)
 
         #this should add the x-values
-        #line_chart.set_categories(x_values)
-        line_chart.add_data(y_values, from_rows = True)
-
-        ws.add_chart(line_chart, "H34")
-        break
-
-
-    #Chat-GPT says that set_categories has to take in numbers, and not text...
-
-
-    #IT TRUNCATES THEM WHEN IT DETECTS IT CAN BE ONE REFERENCE
-    #series Reference for SCDB100 looks like this
-    #'Inventory Analytics.xlsx'!$I$3:$CG$3
-
-    #series Reference for SCDB400 looks like this
-    #'Inventory Analytics.xlsx'!$I$4:$CG$4
-
-    #SEE THIS?, THIS IS THE AXIS LABEL RANGE, THIS IS YOUR X VALUES. SERIES VALUES ARE THE Y VALUES
-    #AHA
-    #='Inventory Analytics.xlsx'!$I$2:$CG$2
+        line_chart.add_data(y_values, titles_from_data = True)
+        line_chart.set_categories(x_values)
+        
+        #makes each individual chart and offsets the position for the next one
+        ws.add_chart(line_chart, get_column_letter(graph_col_offset) + str(PDF.pdf_id + 3 + graph_row_offset))
+        graph_col_offset += 10
+        #starts drawing graphs lower on the screen instead of more horizontally
+        if graph_col_offset > 28:
+            graph_col_offset = 8
+            #the '16' should be the height of each graph
+            graph_row_offset += 16
 
 
 
